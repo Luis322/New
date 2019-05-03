5377e1a297d787536308de912ac0177a078e7ad4
Luis322 <37373448+Luis322@users.noreply.github.com>
2019-05-03 11:04:39 -0500

    Add files via upload

--- resolucion pract 2abril/main.cpp
+++ resolucion pract 2abril/main.cpp
@@ -16,17 +16,39 @@ int sumRec(int *a,int len){
     return sumRec(a,len--)+ *b;
 }
 void printPoint(Point *a,int len){
+    cout<<"[";
     for(int i=0;i<len;i++){
 
-        cout<<a->getX()<<endl;
+        cout<<"("<<a->getX()<<","<<a->getY()<<")";
+        if(i<len-1)
+            cout<<",";
+        *a++;
+    }
+    cout<<"]"<<endl;
+}
+void invertir(int a[], int len){
+    int *b=&a[len-1];
+    for (int i=0;i<(len/2);i++){
+        swap(*a,*b);
+        /*int m=*a;
+        *a=*b;
+        *b=m;*/
+        a++;
+        b--;
     }
 }
-/*void invertir(int *a, int len){
-    int *b=len;
-    for (int i=0;i<len<i++){
-        swap(&a,&b);
-    }
-}*/
+void invertir1(int a[],int len){
+    int *b=a+len-1;
+    for(;b>a;a++,b--)
+        swap(a,b);
+}
+void inverRecu(int a[], int len){
+    if(len==1)
+        return ;
+    swap(a++,(a+len-1));
+    inverRecu(a,len-2) ;
+
+}
 
 int main()
 {
@@ -36,8 +58,16 @@ int main()
     cout<<a<<endl;
     cout<<b<<endl;
     int arr[5]={1,2,3,4,5};
-    cout<<sumRec(arr,5);
+    cout<<sumRec(arr,5)<<endl;
     Point m(1,2),n(3,4),o(5,6),p(7,8);
-    Point arrp[]={m,n,o,p};
+    Point arrp[4]={m,n,o,p};
+    printPoint(arrp,4);
+    //invertir(arr,5);
+    //invertir1(arr,5);
+    inverRecu(arr,5);
+    for(int i=0;i<5;i++)
+        cout<<arr[i]<<endl;
+
+
     return 0;
 }
