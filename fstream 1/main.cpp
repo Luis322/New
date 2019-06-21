3bfe704ccba062884202f426c428081d47e09b7a
Luis322 <37373448+Luis322@users.noreply.github.com>
2019-06-21 10:31:39 -0500

    implementacion para mas lineas

--- fstream 1/main.cpp
+++ fstream 1/main.cpp
@@ -6,25 +6,34 @@ using namespace std;
 int main()
 {
     ofstream archivo("archivo.txt");
-    archivo<<"ciencia de la computacion ciencia de la computacion distribuida";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+    archivo<<"ciencia de la computacion ciencia de la computacion distribuida\n";
+
     archivo.close();
     ifstream archivo1("archivo.txt");
     string texto;
-    //size_t n[50];
-    getline(archivo1,texto);
-    cout<<texto<<endl;
-
+    //int line;
+    //while(!archivo.eof()){
+        getline(archivo1,texto,'\0');
+        cout<<texto<<endl;
+    //}
     ofstream copia("copia.txt");
     copia<<texto;
     copia.close();
+    //line+=1;
+
     ///convertir string a char*
     char cstr[texto.size() + 1];
 	strcpy(cstr, texto.c_str());
 	cout<<texto<<endl;
 	///contando palabras
-    int cont=1;
+    int cont=0;
     for(int i=0;i<texto.size();i++){
-        if(cstr[i]==32)
+        if(cstr[i]==32||cstr[i]=='\n')
             cont+=1;
     }
 	cout<<"El archivo tiene "<<cont<<" palabras"<<endl;
