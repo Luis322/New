63105eada7165f54d4dd732fe275be9d59573483
Luis322 <37373448+Luis322@users.noreply.github.com>
2019-10-29 00:51:08 -0500

    Update Administrativo.cpp

--- Base Datos Colegio/src/Administrativo.cpp
+++ Base Datos Colegio/src/Administrativo.cpp
@@ -2,10 +2,19 @@
 
 Administrativo::Administrativo()
 {
-    //ctor
+    nombre="NULL";
+    edad = 00;
+    codigo = 0;
+    grado=0;
+}
+Administrativo::Administrativo(string _nombre,int _edad,string _cargo):Persona(_nombre,_edad)
+{
+    cargo=_cargo;
+}
+void Administrativo::info()
+{
+    cout<<nombre<<endl;
+    cout<<edad<<endl;
+    cout<<cargo<<endl;
 }
 
-Administrativo::~Administrativo()
-{
-    //dtor
-}
