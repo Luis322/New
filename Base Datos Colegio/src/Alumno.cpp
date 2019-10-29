b43831f32f2dbea67b6fed63c948fbe8b5eaa915
Luis322 <37373448+Luis322@users.noreply.github.com>
2019-10-29 00:52:04 -0500

    Update Alumno.cpp

--- Base Datos Colegio/src/Alumno.cpp
+++ Base Datos Colegio/src/Alumno.cpp
@@ -2,6 +2,20 @@
 
 Alumno::Alumno()
 {
-    //ctor
+    nombre="NULL";
+    edad = 00;
+    codigo = 0;
+    grado=0;
 }
+Alumno::Alumno(string _nombre,int _edad, int _grado):Persona(_nombre,_edad)
+{
+    grado=_grado; 
+}
+void Alumno::info()
+{
+    cout<<nombre<<endl;
+    cout<<edad<<endl;
+    cout<<grado<<endl;
+}
+    
 
